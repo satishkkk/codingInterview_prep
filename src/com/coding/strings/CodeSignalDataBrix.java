daf8a082224a216e71c3a4f7bcb12e0a039a2270
Satish Kathiriya <satishkathiriya99@gmail.com>
2021-10-10 17:13:42 -0700

    16_09 : sync

---
+++ src/com/coding/strings/CodeSignalDataBrix.java
@@ -0,0 +1,31 @@
+package com.coding.strings;
+
+public class CodeSignalDataBrix {
+    public static void main(String[] args) {
+        String out = getOutput("WWDTDTDTWW");
+        System.out.println(out);
+    }
+    
+    static String getOutput(String s){
+        StringBuilder out = new StringBuilder();
+        StringBuilder sb = new StringBuilder(s);
+        
+        while(sb.length() !=0){
+            if(sb.indexOf("W")!=-1){
+                sb.replace(sb.indexOf("W"),sb.indexOf("W"),"");
+                out.append("W");
+            }
+            if(sb.indexOf("D")!=-1){
+                sb.replace(sb.indexOf("D"),sb.indexOf("D"),"");
+                out.append("D");
+            }
+            if(sb.indexOf("T")!=-1){
+                sb.replace(sb.indexOf("T"),sb.indexOf("T"),"");
+                out.append("T");
+            }
+        }
+        return out.toString();
+    }
+    
+    
+}
