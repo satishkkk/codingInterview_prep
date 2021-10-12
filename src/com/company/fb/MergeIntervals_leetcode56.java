237d8553fd817eff56cf78e5907ac38ebfee2c28
Satish Kathiriya <satishkathiriya99@gmail.com>
2021-10-11 21:58:21 -0700

    16_09 : sync

---
+++ src/com/company/fb/MergeIntervals_leetcode56.java
@@ -0,0 +1,16 @@
+package com.company.fb;
+
+import java.util.Arrays;
+
+public class MergeIntervals_leetcode56 {
+    public static void main(String[] args) {
+
+        int[][] a = {{1,3},{8,10},{2,6},{15,18}};
+        int[][] out = merge(a);
+    }
+
+    public static int[][] merge(int[][] intervals) {
+
+return null;
+    }
+}
